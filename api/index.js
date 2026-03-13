69069d7b33c66ac3bfc35ba6b120665c579ddde7
Karl Horky <karl.horky@gmail.com>
2026-03-13 08:21:24 +0100

    Try again for simple ESM + TS solution

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('node:path');
-const { createRequestHandler } = require('expo-server/adapter/vercel');
-
-module.exports = createRequestHandler({
-  build: path.join(__dirname, '../dist/server'),
-});
